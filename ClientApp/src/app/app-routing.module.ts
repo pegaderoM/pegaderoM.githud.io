617dec6b6a711c7f7b2d400c95e635ed23b4fb33
AnthonySibaja <sibajagranados@gmail.com>
2023-05-11 19:11:19 -0600

    first commit

---
+++ ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'pages', loadChildren: () => import('./components/pages/pages.module').then(x => x.PagesModule) },
+  {path:'**',component:NotFoundComponent,pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
